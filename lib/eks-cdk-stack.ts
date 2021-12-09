7b1a01f3d51b4d895afffaecc5ed205d5a916c1b
flavio_p_v <flaviopv@gmail.com>
2021-12-09 14:11:27 -0300

    CDK sample to create eks cluster in AWS using a specific pod with docker image in ECR

--- lib/eks-cdk-stack.ts
+++ lib/eks-cdk-stack.ts
@@ -1,15 +1,55 @@
-import * as cdk from '@aws-cdk/core';
-// import * as sqs from '@aws-cdk/aws-sqs';
+import * as cdk from "@aws-cdk/core";
+import * as autoscaling from "@aws-cdk/aws-autoscaling";
+import * as iam from "@aws-cdk/aws-iam";
+import * as ec2 from "@aws-cdk/aws-ec2";
+import * as eks from "@aws-cdk/aws-eks";
+
 
 export class EksCdkStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const vpc = new ec2.Vpc(this, 'EKSVpc');  // Create a new VPC for our cluster
+    
+    // IAM role for our EC2 worker nodes
+    const workerRole = new iam.Role(this, 'EKSWorkerRole', {
+      assumedBy: new iam.ServicePrincipal('ec2.amazonaws.com')
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'EksCdkQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const eksCluster = new eks.Cluster(this, 'Cluster', {
+      vpc: vpc,
+      defaultCapacity: 0,  // we want to manage capacity our selves
+      version: eks.KubernetesVersion.V1_21,
+    });
+
+    eksCluster.addManifest('mypod', {
+      apiVersion: 'v1',
+      kind: 'Pod',
+      metadata: { name: 'mypod' },
+      spec: {
+        containers: [
+          {
+            name: 'testapp',
+            image: '591466733858.dkr.ecr.sa-east-1.amazonaws.com/test-repository:latest',
+            ports: [ { containerPort: 3000 } ],
+          },
+        ],
+      },
+    });
+
+    const onDemandASG = new autoscaling.AutoScalingGroup(this, 'OnDemandASG', {
+      vpc: vpc,
+      role: workerRole,
+      minCapacity: 1,
+      maxCapacity: 2,
+      instanceType: new ec2.InstanceType('t3.medium'),
+      machineImage: new eks.EksOptimizedImage({
+        kubernetesVersion: '1.21',
+        nodeType: eks.NodeType.STANDARD  // without this, incorrect SSM parameter for AMI is resolved
+      }),
+      updatePolicy: autoscaling.UpdatePolicy.rollingUpdate()
+      });
+
+    eksCluster.connectAutoScalingGroupCapacity(onDemandASG, {});
   }
 }
